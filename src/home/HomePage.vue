bd0b552250ce701d788892dfa3884a4c0d9ddc7e
Markus Felten <Markus.Felten@gmx.de>
2019-01-28 21:07:14 +0100

    style: reformat only

--- src/home/HomePage.vue
+++ src/home/HomePage.vue
@@ -2,7 +2,6 @@
     <div>
         <h1>Hi {{user.firstName}}!</h1>
         <p>You're logged in with Vue + Vuex & JWT!!</p>
-        <h3>Users from secure api end point:</h3>
         <em v-if="users.loading">Loading users...</em>
         <span v-if="users.error" class="text-danger">ERROR: {{users.error}}</span>
         <ul v-if="users.items">
