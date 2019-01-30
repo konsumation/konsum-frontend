b4b22a642ee40cf22d34cf2f9a5db74155b17384
Markus Felten <Markus.Felten@gmx.de>
2019-01-30 18:23:36 +0100

    refactor: use async functions

--- src/_services/user.service.js
+++ src/_services/user.service.js
@@ -7,24 +7,22 @@ export const userService = {
   getAll
 };
 
-function login(username, password) {
+async function login(username, password) {
   const requestOptions = {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ username, password })
   };
 
-  return fetch(`${config.apiUrl}/authenticate`, requestOptions)
-    .then(handleResponse)
-    .then(user => {
-      // login successful if there's a jwt token in the response
-      if (user.token) {
-        // store user details and jwt token in local storage to keep user logged in between page refreshes
-        localStorage.setItem("user", JSON.stringify(user));
-      }
+  const response = await fetch(`${config.apiUrl}/authenticate`, requestOptions);
+  const user = await handleResponse(response);
+  // login successful if there's a jwt token in the response
+  if (user.token) {
+    // store user details and jwt token in local storage to keep user logged in between page refreshes
+    localStorage.setItem("user", JSON.stringify(user));
+  }
 
-      return user;
-    });
+  return user;
 }
 
 function logout() {
@@ -38,23 +36,22 @@ function getAll() {
     headers: authHeader()
   };
 
-  return fetch(`${config.apiUrl}/users`, requestOptions).then(handleResponse);
+  return fetch(`${config.apiUrl}/values`, requestOptions).then(handleResponse);
 }
 
-function handleResponse(response) {
-  return response.text().then(text => {
-    const data = text && JSON.parse(text);
-    if (!response.ok) {
-      if (response.status === 401) {
-        // auto logout if 401 response returned from api
-        logout();
-        location.reload(true);
-      }
+async function handleResponse(response) {
+  const text = await response.text();
 
-      const error = (data && data.message) || response.statusText;
-      return Promise.reject(error);
+  const data = text && JSON.parse(text);
+  if (!response.ok) {
+    if (response.status === 401) {
+      // auto logout if 401 response returned from api
+      logout();
+      location.reload(true);
     }
 
-    return data;
-  });
+    throw (data && data.message) || response.statusText;
+  }
+
+  return data;
 }
--- src/home/HomePage.vue
+++ src/home/HomePage.vue
@@ -1,32 +1,32 @@
 <template>
-    <div>
-        <h1>Hi {{user.firstName}}!</h1>
-        <p>You're logged in with Vue + Vuex & JWT!!</p>
-        <em v-if="users.loading">Loading users...</em>
-        <span v-if="users.error" class="text-danger">ERROR: {{users.error}}</span>
-        <ul v-if="users.items">
-            <li v-for="user in users.items" :key="user.id">
-                {{user.firstName + ' ' + user.lastName}}
-            </li>
-        </ul>
-        <p>
-            <router-link to="/login">Logout</router-link>
-        </p>
-    </div>
+  <div>
+    <h1>Hi</h1>
+    <p>You're logged in to Konsum</p>
+    <em v-if="users.loading">Loading users...</em>
+    <span v-if="users.error" class="text-danger">ERROR: {{ users.error }}</span>
+    <ul v-if="users.items">
+      <li v-for="user in users.items" :key="user.id">
+        {{ user.firstName + " " + user.lastName }}
+      </li>
+    </ul>
+    <p>
+      <router-link to="/login">Logout</router-link>
+    </p>
+  </div>
 </template>
 
 <script>
 export default {
-    computed: {
-        user () {
-            return this.$store.state.authentication.user;
-        },
-        users () {
-            return this.$store.state.users.all;
-        }
+  computed: {
+    user() {
+      return this.$store.state.authentication.user;
     },
-    created () {
-        this.$store.dispatch('users/getAll');
+    users() {
+      return this.$store.state.users.all;
     }
+  },
+  created() {
+    this.$store.dispatch("users/getAll");
+  }
 };
 </script>
