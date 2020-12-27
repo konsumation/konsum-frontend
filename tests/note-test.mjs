7fea49892be8b5786894d2c6c87b89277029ccbd
Markus Felten <Markus.Felten@gmx.de>
2020-12-27 17:32:25 +0100

    add missing tests/note-test.mjs from template

---
+++ tests/note-test.mjs
@@ -0,0 +1,43 @@
+import test from "ava";
+import tmp from "tmp";
+import levelup from "levelup";
+import leveldown from "leveldown";
+import { secondsAsString } from "../src/util.mjs";
+import { Master, Category, Note } from "konsum-db";
+
+test("create Note", async t => {
+  const time = Date.now();
+  const master = {};
+
+  const c = new Category("CAT-1", master);
+  const n1 = new Note(time, c, { description: "some text" });
+
+  t.is(n1.keyPrefix, "notes.CAT-1.");
+  t.is(n1.key, "notes.CAT-1." + secondsAsString(time));
+  t.is(n1.description, "some text");
+});
+
+test("Note write / read", async t => {
+  const master = await Master.initialize(await levelup(leveldown(tmp.tmpNameSync())));
+
+  const c = new Category("CAT-1", master, { unit: "kWh", fractionalDigits: 3 });
+  await c.write(master.db);
+
+  const time = Date.now();
+
+  for (let i = 0; i < 5; i++) {
+    const n = new Note(time + i, c, { description: `description ${i}` });
+    await n.write(master.db);
+  }
+
+  const ms = [];
+
+  for await (const m of Note.entriesWith(master.db, c)) {
+    ms.push(m);
+  }
+
+  t.true(ms.length >= 5);
+  t.is(ms[0].description, "description 0");
+
+  master.close();
+});
