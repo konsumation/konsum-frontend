5fb20759bbf5322d2dfadeb76ac126912abd3a02
Markus Felten <Markus.Felten@gmx.de>
2020-12-27 17:32:18 +0100

    add missing tests/master-test.mjs from template

---
+++ tests/master-test.mjs
@@ -0,0 +1,75 @@
+import test from "ava";
+import tmp from "tmp";
+import levelup from "levelup";
+import leveldown from "leveldown";
+
+import { Master, Category, Meter } from "konsum-db";
+import { createWriteStream, createReadStream } from "fs";
+import { stat } from "fs/promises";
+
+test("initialize", async t => {
+  const db = await levelup(leveldown(tmp.tmpNameSync()));
+  const master = await Master.initialize(db);
+
+  t.is(master.schemaVersion, "1");
+  t.is(master.db, db);
+
+  const categories = [];
+  for await (const c of master.categories()) {
+    categories.push(c);
+  }
+  t.deepEqual(categories, []);
+
+  db.close();
+});
+
+const SECONDS_A_DAY = 60 * 60 * 24;
+
+test("backup", async t => {
+  const master = await Master.initialize(
+    await levelup(leveldown(tmp.tmpNameSync()))
+  );
+
+  for (let i = 0; i < 3; i++) {
+    const c = new Category(`CAT-${i}`, master, {
+      unit: "kWh",
+      fractionalDigits: 2,
+      description: "mains power"
+    });
+
+    await c.write(master.db);
+    const m1 = new Meter("M-1", c, { serial: "1" });
+    m1.write(master.db);
+    const m2 = new Meter("M-2", c, { serial: "2" });
+    m2.write(master.db);
+
+    const first = Date.now();
+    const firstValue = 77.34 + i;
+    let last = first;
+    let lastValue = firstValue;
+
+    for (let i = 0; i < 10; i++) {
+      last = new Date(first + SECONDS_A_DAY * i).getTime();
+      lastValue = firstValue + i;
+      await c.writeValue(master.db, lastValue, last);
+    }
+  }
+
+  const ofn = tmp.tmpNameSync();
+  const out = createWriteStream(ofn, { encoding: "utf8" });
+
+  await master.backup(out);
+
+  const s = await stat(ofn);
+
+  console.log(ofn);
+  t.is(s.size, 620);
+  master.close();
+
+  const master2 = await Master.initialize(
+    await levelup(leveldown(tmp.tmpNameSync()))
+  );
+  const input = createReadStream(ofn, { encoding: "utf8" });
+
+  await master2.restore(input);
+});
