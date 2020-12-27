90a873283f2f5b5210be1e30153f584258340faa
Markus Felten <Markus.Felten@gmx.de>
2020-12-27 17:32:29 +0100

    add missing tests/restore-version-1.mjs from template

---
+++ tests/restore-version-1.mjs
@@ -0,0 +1,27 @@
+import test from "ava";
+import tmp from "tmp";
+import levelup from "levelup";
+import leveldown from "leveldown";
+
+import { Master, Category } from "konsum-db";
+import { createReadStream } from "fs";
+
+test("restore version 1", async t => {
+  const fixture = new URL("fixtures/database-version-1.txt", import.meta.url);
+  const db = await levelup(leveldown(tmp.tmpNameSync()));
+  const master = await Master.initialize(db);
+
+  const input = createReadStream(fixture.pathname, { encoding: "utf8" });
+
+  await master.restore(input);
+
+  const categories = [];
+  for await (const c of Category.entries(db)) {
+    categories.push(c);
+  }
+
+  t.deepEqual(
+    categories.map(c => c.name),
+    ["CAT-0", "CAT-1", "CAT-2"]
+  );
+});
